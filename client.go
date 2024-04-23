16ab8ee4f9f20f69b51abbc7f03479efa0d5251b
Felipe Nascimento <flpnascto@gmail.com>
2024-04-23 08:18:04 -0300

    feat: write and read channel

--- client.go
+++ client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"time"
@@ -12,17 +13,21 @@ func main() {
 	ctx, cancel := context.WithTimeout(mainCtx, 1*time.Second)
 	defer cancel()
 
+	c1 := make(chan string)
+
 	select {
 	case <-ctx.Done():
 		println("Tempo de 1 segundo excedido")
 		return
 	default:
-		fetchBrasilApi("13330250")
-		fetchViaCepApi("13330250")
+		go fetchBrasilApi("13330250", c1)
+		go fetchViaCepApi("13330250", c1)
 	}
+
+	readChannel(c1)
 }
 
-func fetchBrasilApi(cep string) {
+func fetchBrasilApi(cep string, c chan string) {
 	endpoint := "https://brasilapi.com.br/api/cep/v1/" + cep
 	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
@@ -37,11 +42,11 @@ func fetchBrasilApi(cep string) {
 	if err != nil {
 		panic(err)
 	}
-	println(string(body))
-	// TODO escrever responsta no email.
+	result := "BrasilApi:\n" + string(body)
+	channelReciver(result, c)
 }
 
-func fetchViaCepApi(cep string) {
+func fetchViaCepApi(cep string, c chan string) {
 	endpoint := "http://viacep.com.br/ws/" + cep + "/json"
 	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
@@ -56,6 +61,14 @@ func fetchViaCepApi(cep string) {
 	if err != nil {
 		panic(err)
 	}
-	println(string(body))
-	// TODO escrever responsta no email.
+	result := "ViaCepApi:\n" + string(body)
+	channelReciver(result, c)
+}
+
+func channelReciver(data string, c chan<- string) {
+	c <- data
+}
+
+func readChannel(data chan string) {
+	fmt.Println(<-data)
 }
